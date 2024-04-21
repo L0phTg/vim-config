2e63ec22df9aa375acb4c7b2c6b106d88781046b
L0phTg <L0phTg@163.com>
2024-04-21 11:09:18 +0800

    fixs: indent, sniprun plugin configs

--- lua/indent-dot.lua
+++ lua/indent-dot.lua
@@ -5,7 +5,7 @@
     --show_end_of_line = true,
 --}
 
-local status_ok, indent_blankline = pcall(require, "indent_blankline")
+local status_ok, indent_blankline = pcall(require, "ibl")
 if not status_ok then
 	return
 end
@@ -67,7 +67,7 @@ vim.wo.colorcolumn = "99999"
 indent_blankline.setup({
 	-- show_end_of_line = true,
 	-- space_char_blankline = " ",
-	show_current_context = true,
+	-- show_current_context = true,
 	-- show_current_context_start = true,
 	-- char_highlight_list = {
 	--   "IndentBlanklineIndent1",
